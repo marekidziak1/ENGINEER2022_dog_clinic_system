0a5e93c2c593f4f002758e6b656f54d0b1fbaf93
unknown <dominikspiewak97@gmail.com>
2022-04-24 16:45:29 +0200

    feat: add swiper reservation calendar - styles completed, to do js logic

--- js/reservation/reservation.js
+++ js/reservation/reservation.js
@@ -1,3 +1,36 @@
-$(function () {
- 
+$(document).ready(function(){
+    var swiperTermsCalendar = new Swiper('.js-terms-calendar', {
+        //watchOverflow: true,
+        // spaceBetween: 0,
+        // centeredSlides: true,
+        // loop: true,
+        slidesPerView: 2,
+        // slidesPerGroup: 2,  
+        navigation: {
+            prevEl: '.js-terms-calendar--prev',
+            nextEl: '.js-terms-calendar--next',
+        },
+        breakpoints: {
+            551: {
+            slidesPerView: 3,
+            slidesPerGroup: 4,
+            // spaceBetween: 60,
+            },
+            850: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            //spaceBetween: 30,
+            },
+            1024: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            //spaceBetween: 30,
+            },
+            1201: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+                //spaceBetween: 30,
+            },
+        }             
+     });
 });
