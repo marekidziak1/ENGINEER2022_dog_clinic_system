18d1b1dca75b73a6777a36b354fb6567c8a3a606
MAREK <marekidziak1@gmail.com>
2022-05-16 20:35:47 +0300

    user registration cancel email field

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -6,10 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model=User
-        fields = ['username', 'email','password1','password2']
+        fields = ['username','password1','password2']
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'g__form-input input'})
-        self.fields['email'].widget.attrs.update({'class': 'g__form-input input'})
-        self.fields['password1'].widget.attrs.update({'class': 'g__form-input input'})
-        self.fields['password2'].widget.attrs.update({'class': 'g__form-input input'})
+        self.fields['username'].widget.attrs.update({'class': 'g__form-input input', 'type':"text"})
+        self.fields['password1'].widget.attrs.update({'class': 'g__form-input input', 'type':"text"})
+        self.fields['password2'].widget.attrs.update({'class': 'g__form-input input', 'type':"text"})
