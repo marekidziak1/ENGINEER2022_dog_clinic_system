e9e9f5cdfda1789700d640c0956802925726eba6
MAREK <marekidziak1@gmail.com>
2022-05-17 01:21:56 +0300

    complete login view function

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -1,14 +1,23 @@
 from django import forms
+from django.forms.widgets import PasswordInput, TextInput
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(widget=TextInput(attrs={'class': 'g__form-input input','placeholder': 'username','type':"text"}))
+    password1 = forms.CharField(widget=PasswordInput(attrs={'class': 'g__form-input input','placeholder':'type password','type':"password"}))
+    password2 = forms.CharField(widget=PasswordInput(attrs={'class': 'g__form-input input','placeholder':'confirm password','type':"password"}))
     class Meta:
         model=User
         fields = ['username','password1','password2']
-    def __init__(self, *args, **kwargs):
-        super(UserRegisterForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'g__form-input input', 'type':"text"})
-        self.fields['password1'].widget.attrs.update({'class': 'g__form-input input', 'type':"text"})
-        self.fields['password2'].widget.attrs.update({'class': 'g__form-input input', 'type':"text"})
+    # def __init__(self, *args, **kwargs):
+    #     super(UserRegisterForm, self).__init__(*args, **kwargs)
+    #     self.fields['username'].widget.attrs.update({'class': 'g__form-input input', 'type':"text"})
+    #     self.fields['password1'].widget.attrs.update({'class': 'g__form-input input', 'type':"password"})
+    #     self.fields['password2'].widget.attrs.update({'class': 'g__form-input input', 'type':"password"})
+
+class UserAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput(attrs={'class': 'g__form-input input','placeholder': 'username','type':"text"}))
+    password = forms.CharField(widget=PasswordInput(attrs={'class': 'g__form-input input','placeholder':'password','type':"password"}))
+    class Meta:
+        model = User
+        fields = ['username','password']
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,10 @@
-from codecs import utf_16_be_decode
-from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
-from django.contrib.auth import authenticate, login
+from .forms import UserAuthenticationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+
 # Create your views here.
 def registerView(request):
     form = UserRegisterForm()
@@ -17,15 +19,26 @@ def registerView(request):
                 username = form.cleaned_data.get('username')
                 password = form.cleaned_data.get('password1')
                 user = authenticate(request, username=username, password = password)
+                if user is not None:
+                    login(request, user)
+                    messages.info(request, 'Użytkownik utworzony i zalogowany')
+                    return redirect('home')
             except:
-                messages.error(request, 'Użytkownik utworzony ale nie można się zalogować')
+                messages.error(request, 'Użytkownik utworzony ale nie można było się zalogować')
                 return redirect('login_user')
-            if user is not None:
-                login(request, user)
-                return redirect('home')
     context={'form':form}
     return render(request, 'users/register.html', context)
 
 def loginView(request):
-    context={}
-    return render(request,"users/login.html" ,context)
+    form= UserAuthenticationForm()
+    if request.method == "POST":
+        form = UserAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()                 
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Zła nazwa użytkownika lub hasło')
+    context={'form':form}
+    return render(request, "users/login.html", context)
+
